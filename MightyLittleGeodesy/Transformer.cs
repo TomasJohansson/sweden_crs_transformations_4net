135e543dc8ae7fd7b8c7be9fc0f034896daea7b4
Tomas Johansson <github-tomas@programmerare.com>
2021-01-09 00:24:58 +0100

    Added a file with coordinates in Sweden to be used for regression testing, and implemented a test method which verifies the transformations for this current implementation. The current implementation use the three classes RT90Position, SWEREF99Position, WGS84Position, from within a new class Transformer with implementation that selects which of these classes to use based on the EPSG code for the source and target coordinates.

---
+++ MightyLittleGeodesy/Transformer.cs
@@ -0,0 +1,90 @@
+﻿using MightyLittleGeodesy.Positions;
+using System;
+using static MightyLittleGeodesy.Positions.RT90Position;
+using static MightyLittleGeodesy.Positions.SWEREF99Position;
+
+namespace MightyLittleGeodesy
+{
+
+    public class Transformer {
+        public const int epsgForWgs84 = 4326;
+        private const int epsgForSweref99tm = 3006;
+
+        //private const int epsgLowerValueForSwerefLocal = 3007; // the NATIONAL sweref99TM has value 3006 as in the above constant
+        //private const int epsgUpperValueForSwerefLocal = 3018;
+        private const int epsgLowerValueForSweref = epsgForSweref99tm;
+        private const int epsgUpperValueForSweref = 3018;
+
+        private const int epsgLowerValueForRT90 = 3019;
+        private const int epsgUpperValueForRT90 = 3024;
+
+        public static Coordinate Transform(Coordinate sourceCoordinate, int targetEpsg) {
+            if(sourceCoordinate.epsgNumber == targetEpsg) throw new ArgumentException("Trying to transform from/to the same CRS");
+            if(isWgs84(sourceCoordinate.epsgNumber)) {
+                var wgs84position = new WGS84Position(sourceCoordinate.yLatitude, sourceCoordinate.xLongitude);
+                if(isSweref(targetEpsg)) {
+                    SWEREFProjection swerefProjection = ProjectionFactory.GetSwerefProjectionByEpsgNumber(targetEpsg);
+                    var position = new SWEREF99Position(wgs84position, swerefProjection);
+                    return new Coordinate(targetEpsg, position.Longitude, position.Latitude);
+                }
+                else if(isRT90(targetEpsg)) {
+                    RT90Projection rt90Projection = ProjectionFactory.GetRT90ProjectionProjectionByEpsgNumber(targetEpsg);
+                    var position = new RT90Position(wgs84position, rt90Projection);
+                    return new Coordinate(targetEpsg, position.Longitude, position.Latitude);
+                }
+            }
+            else if(isSweref(sourceCoordinate.epsgNumber)) {
+                if(isWgs84(targetEpsg)) {
+                    SWEREFProjection swerefProjection = ProjectionFactory.GetSwerefProjectionByEpsgNumber(sourceCoordinate.epsgNumber);
+                    var sweref99Position = new SWEREF99Position(sourceCoordinate.yLatitude, sourceCoordinate.xLongitude, swerefProjection);
+                    var wgs84result = sweref99Position.ToWGS84();
+                    return new Coordinate(targetEpsg, wgs84result.Longitude, wgs84result.Latitude);
+                }
+                else {
+                    // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
+                    var wgs84coordinate = Transform(sourceCoordinate, epsgForWgs84);
+                    return Transform(wgs84coordinate, targetEpsg);
+                }
+            }
+            else if(isRT90(sourceCoordinate.epsgNumber)) {
+                if(isWgs84(targetEpsg)) {
+                    RT90Projection rt90Projection = ProjectionFactory.GetRT90ProjectionProjectionByEpsgNumber(sourceCoordinate.epsgNumber);
+                    var rt90Position = new RT90Position(sourceCoordinate.yLatitude, sourceCoordinate.xLongitude, rt90Projection);
+                    var wgs84result = rt90Position.ToWGS84();
+                    return new Coordinate(targetEpsg, wgs84result.Longitude, wgs84result.Latitude);
+                }
+                else {
+                    // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
+                    var wgs84coordinate = Transform(sourceCoordinate, epsgForWgs84);
+                    return Transform(wgs84coordinate, targetEpsg);
+                }
+            }
+            throw new ArgumentException(string.Format("Unhandled source/target EPSG {0} ==> {1}", sourceCoordinate.epsgNumber, targetEpsg));
+        }
+
+        private static bool isWgs84(int epsgNumber) {
+            return epsgNumber == epsgForWgs84;
+        }
+        private static bool isSweref(int epsgNumber) {
+            return epsgLowerValueForSweref <= epsgNumber && epsgNumber <= epsgUpperValueForSweref;
+        }
+        private static bool isRT90(int epsgNumber) {
+            return epsgLowerValueForRT90 <= epsgNumber && epsgNumber <= epsgUpperValueForRT90;
+        }
+    }
+
+    public class Coordinate {
+        public readonly int epsgNumber;
+        public readonly double xLongitude;
+        public readonly double yLatitude;
+        public Coordinate(
+            int epsgNumber,
+            double xLongitude,
+            double yLatitude
+        ) {
+            this.epsgNumber = epsgNumber;
+            this.xLongitude = xLongitude;
+            this.yLatitude = yLatitude;
+        }
+    }
+}
