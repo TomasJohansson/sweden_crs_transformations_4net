6ace16eebcc4575477760a08bdc8171a21f6ba14
Tomas Johansson <github-tomas@programmerare.com>
2021-01-09 00:06:10 +0100

    Added a class (and a test class for it) with methods for getting all projections, and for getting one projection by its EPSG number.

---
+++ MightyLittleGeodesy/ProjectionFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static MightyLittleGeodesy.Positions.RT90Position;
+using static MightyLittleGeodesy.Positions.SWEREF99Position;
+
+namespace MightyLittleGeodesy {
+    
+    /// <summary>
+    /// Class with methods for getting all projections, and for getting one projection by its EPSG number.
+    /// </summary>
+    public static class ProjectionFactory {
+        public static SWEREFProjection GetSwerefProjectionByEpsgNumber(int epsg) {
+            var values = GetAllSwerefProjections();
+            foreach(SWEREFProjection value in values) {
+                if(value.GetEpsgNumber() == epsg) {
+                    return value;
+                }
+            }
+            throw new ArgumentException("Could not find SWEREFProjection for EPSG " + epsg);
+        }
+        public static RT90Projection GetRT90ProjectionProjectionByEpsgNumber(int epsg) {
+            var values = GetAllRT90Projections();
+            foreach(RT90Projection value in values) {
+                if(value.GetEpsgNumber() == epsg) {
+                    return value;
+                }
+            }
+            throw new ArgumentException("Could not find RT90Projection for EPSG " + epsg);
+        }
+
+        public static IList<SWEREFProjection> GetAllSwerefProjections() {
+            return ((SWEREFProjection[])Enum.GetValues(typeof(SWEREFProjection))).ToList();
+        }
+        public static IList<RT90Projection> GetAllRT90Projections() {
+            return ((RT90Projection[])Enum.GetValues(typeof(RT90Projection))).ToList();
+        }
+    }
+
+    public static class ProjectionEnumExtensions {
+        public static int GetEpsgNumber(this SWEREFProjection swerefProjection) {
+            // The values in the enum happen to be defined in the same order as the EPSG number i.e. the enum values:
+            //sweref_99_tm = 0,
+            //sweref_99_12_00 = 1,
+            //...
+            //sweref_99_21_45 = 11,
+            //sweref_99_23_15 = 12
+
+            // while the correspondign EPSG values are (in the same order) 3006-3018, so therefore add 3006 to the value
+            int theEnumValue = (int)swerefProjection;
+            return theEnumValue + 3006;
+        }
+        public static int GetEpsgNumber(this RT90Projection rt90Projection) {
+            // The values in the enum happen to be defined in the same order as the EPSG number i.e. the enum values:
+            //rt90_7_5_gon_v = 0,
+            // ...
+            //rt90_5_0_gon_o = 5
+
+            // while the correspondign EPSG values are (in the same order) 3019-3024, so therefore add 3019 to the value
+            int theEnumValue = (int)rt90Projection;
+            return theEnumValue + 3019;
+        }
+    }
+
+}
---
+++ MightyLittleGeodesyTests/ProjectionFactoryTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MightyLittleGeodesy;
+using static MightyLittleGeodesy.Positions.RT90Position;
+using static MightyLittleGeodesy.Positions.SWEREF99Position;
+
+namespace MightyLittleGeodesyTests
+{
+    [TestClass]
+    public class ProjectionFactoryTest {
+
+        private const int epsgNumberForSweref99tm = 3006; // https://epsg.org/crs_3006/SWEREF99-TM.html
+        private const int numberOfSweref99projections = 13; // with EPSG numbers 3006-3018
+        private const int numberOfRTprojections = 6; // with EPSG numbers 3019-3024
+
+        [TestMethod]
+        public void GetSWEREFProjectionByEpsgNumber() {
+            Assert.AreEqual(
+                SWEREFProjection.sweref_99_tm,
+                ProjectionFactory.GetSwerefProjectionByEpsgNumber(epsgNumberForSweref99tm)
+            );
+
+            Assert.AreEqual(
+                SWEREFProjection.sweref_99_23_15,
+                ProjectionFactory.GetSwerefProjectionByEpsgNumber(3018) // https://epsg.io/3018
+            );
+        }
+
+        [TestMethod]
+        public void GetRT90ProjectionProjectionByEpsgNumber() {
+            Assert.AreEqual(
+                RT90Projection.rt90_7_5_gon_v,
+                ProjectionFactory.GetRT90ProjectionProjectionByEpsgNumber(3019) // https://epsg.io/3019
+            );
+
+            Assert.AreEqual(
+                RT90Projection.rt90_5_0_gon_o,
+                ProjectionFactory.GetRT90ProjectionProjectionByEpsgNumber(3024) // https://epsg.io/3024
+            );
+        }
+
+
+        [TestMethod]
+        public void GetAllSwerefProjections() {
+            var allSwerefProjections = ProjectionFactory.GetAllSwerefProjections();
+            Assert.AreEqual(
+                numberOfSweref99projections,
+                allSwerefProjections.Count
+            );
+
+            foreach(var swerefProjection in allSwerefProjections) {
+                var swerefProj = ProjectionFactory.GetSwerefProjectionByEpsgNumber(swerefProjection.GetEpsgNumber());
+                Assert.AreEqual(swerefProjection, swerefProj);
+            }
+        }    
+
+        [TestMethod]
+        public void GetAllRT90Projections() {
+            var allRT90Projections = ProjectionFactory.GetAllRT90Projections();
+            Assert.AreEqual(
+                numberOfRTprojections,
+                allRT90Projections.Count
+            );
+
+            foreach(var rt90Projection in allRT90Projections) {
+                var rt90proj = ProjectionFactory.GetRT90ProjectionProjectionByEpsgNumber(rt90Projection.GetEpsgNumber());
+                Assert.AreEqual(rt90Projection, rt90proj);
+            }
+        }    
+
+
+        // ProjectionEnumExtensions
+        [TestMethod]
+        public void GetEpsgNumber() {
+            Assert.AreEqual(
+                epsgNumberForSweref99tm,
+                SWEREFProjection.sweref_99_tm.GetEpsgNumber()
+            );
+        }
+
+    }
+}
