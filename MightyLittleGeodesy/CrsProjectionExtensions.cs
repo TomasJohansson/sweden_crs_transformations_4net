71a646945c91d417cdc7797b206c06f478fddbc1
Tomas Johansson <github-tomas@programmerare.com>
2021-01-09 01:54:26 +0100

    Renamed the class/file ProjectionEnumExtensions to CrsProjectionExtensions

--- MightyLittleGeodesy/CrsProjectionExtensions.cs
+++ MightyLittleGeodesy/CrsProjectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace MightyLittleGeodesy
 {
-    public static class ProjectionEnumExtensions {
+    public static class CrsProjectionExtensions {
         public static int GetEpsgNumber(this CrsProjection crsProjection) { 
             // the EPSG numbers have been used as the values in this enum (which will replace SWEREFProjection and RT90Projection)
             return (int)crsProjection;
