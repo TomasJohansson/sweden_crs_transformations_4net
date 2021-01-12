6b227ff32fd0a858717833dd28d87280cb46a7ad
Tomas Johansson <github-tomas@programmerare.com>
2021-01-12 01:07:27 +0100

    Refactoring. Removed two duplicated TransformStrategy implementation classes and renamed two remaining TransformStrategy implementation classes.

--- MightyLittleGeodesy/Transformer.cs
+++ MightyLittleGeodesy/Transformer.cs
@@ -6,12 +6,10 @@ namespace MightyLittleGeodesy {
     internal class Transformer {
 
         // Implementations of transformations from WGS84:
-        private static TransformStrategy _transformStrategy_from_WGS84_to_SWEREF99      = new TransformStrategy_from_WGS84_to_SWEREF99();
-        private static TransformStrategy _transFormStrategy_from_WGS84_to_RT90          = new TransformStrategy_from_WGS84_to_RT90();
+        private static TransformStrategy _transformStrategy_from_WGS84_to_SWEREF99_or_RT90 = new TransformStrategy_from_WGS84_to_SWEREF99_or_RT90();
 
         // Implementations of transformations to WGS84:
-        private static TransformStrategy _transformStrategy_from_SWEREF99_to_WGS84      = new TransformStrategy_from_SWEREF99_to_WGS84();
-        private static TransformStrategy _transformStrategy_from_RT90_to_WGS84          = new TransformStrategy_from_RT90_to_WGS84();
+        private static TransformStrategy _transformStrategy_from_SWEREF99_or_RT90_to_WGS84 = new TransformStrategy_from_SWEREF99_or_RT90_to_WGS84();
 
         // Implementation first transforming to WGS84 and then to the real target:
         private static TransformStrategy _transFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget  = new TransFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget();
@@ -23,15 +21,15 @@ namespace MightyLittleGeodesy {
 
             if(sourceCoordinate.crsProjection.isWgs84()) {
                 if(targetCrsProjection.isSweref()) {
-                    _transFormStrategy = _transformStrategy_from_WGS84_to_SWEREF99;
+                    _transFormStrategy = _transformStrategy_from_WGS84_to_SWEREF99_or_RT90;
                 }
                 else if(targetCrsProjection.isRT90()) {
-                    _transFormStrategy = _transFormStrategy_from_WGS84_to_RT90;
+                    _transFormStrategy = _transformStrategy_from_WGS84_to_SWEREF99_or_RT90;
                 }
             }
             else if(sourceCoordinate.crsProjection.isSweref()) {
                 if(targetCrsProjection.isWgs84()) {
-                    _transFormStrategy = _transformStrategy_from_SWEREF99_to_WGS84;
+                    _transFormStrategy = _transformStrategy_from_SWEREF99_or_RT90_to_WGS84;
                 }
                 else {
                     // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
@@ -40,7 +38,7 @@ namespace MightyLittleGeodesy {
             }
             else if(sourceCoordinate.crsProjection.isRT90()) {
                 if(targetCrsProjection.isWgs84()) {
-                    _transFormStrategy = _transformStrategy_from_RT90_to_WGS84;
+                    _transFormStrategy = _transformStrategy_from_SWEREF99_or_RT90_to_WGS84;
                 }
                 else {
                     // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
@@ -62,21 +60,8 @@ namespace MightyLittleGeodesy {
         );
     }
 
-    internal class TransformStrategy_from_WGS84_to_SWEREF99 : TransformStrategy {
-        // Precondition: sourceCoordinate must be CRS WGS84
-        public CrsCoordinate Transform(
-            CrsCoordinate sourceCoordinate,
-            CrsProjection targetCrsProjection
-        ) {
-            var gkProjection = new GaussKreuger();
-            gkProjection.swedish_params(targetCrsProjection);
-            LonLat lonLat = gkProjection.geodetic_to_grid(sourceCoordinate.yLatitude, sourceCoordinate.xLongitude);
-            return CrsCoordinate.CreateCoordinatePoint(targetCrsProjection, lonLat.xLongitude, lonLat.yLatitude);
-        }
-    }
-
     
-    internal class TransformStrategy_from_WGS84_to_RT90 : TransformStrategy {
+    internal class TransformStrategy_from_WGS84_to_SWEREF99_or_RT90 : TransformStrategy {
         // Precondition: sourceCoordinate must be CRS WGS84
         public CrsCoordinate Transform(
             CrsCoordinate sourceCoordinate,
@@ -89,19 +74,7 @@ namespace MightyLittleGeodesy {
         }
     }
 
-    internal class TransformStrategy_from_SWEREF99_to_WGS84 : TransformStrategy {
-        // Precondition: sourceCoordinate must be CRS SWEREF99
-        public CrsCoordinate Transform(
-            CrsCoordinate sourceCoordinate,
-            CrsProjection targetCrsProjection
-        ) {
-            var gkProjection = new GaussKreuger();
-            gkProjection.swedish_params(sourceCoordinate.crsProjection);
-            LonLat lonLat = gkProjection.grid_to_geodetic(sourceCoordinate.yLatitude, sourceCoordinate.xLongitude); 
-            return CrsCoordinate.CreateCoordinatePoint(targetCrsProjection, lonLat.xLongitude, lonLat.yLatitude);
-        }
-    }
-    internal class TransformStrategy_from_RT90_to_WGS84 : TransformStrategy {
+    internal class TransformStrategy_from_SWEREF99_or_RT90_to_WGS84 : TransformStrategy {
         // Precondition: sourceCoordinate must be CRS RT90
         public CrsCoordinate Transform(
             CrsCoordinate sourceCoordinate,
