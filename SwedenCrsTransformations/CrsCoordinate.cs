e27fc1cd9869e9cc8e3e76284281e0be2ef0420c
Tomas Johansson <github-tomas@programmerare.com>
2021-01-12 15:16:46 +0100

    Generated five methods (with Visual Studio 2019) for the class CoordinatePoint, and added a test method.

--- SwedenCrsTransformations/CrsCoordinate.cs
+++ SwedenCrsTransformations/CrsCoordinate.cs
@@ -1,7 +1,9 @@
 ﻿using SwedenCrsTransformations.Transformation;
+using System;
+using System.Collections.Generic;
 
 namespace SwedenCrsTransformations {
-    public class CrsCoordinate {
+    public class CrsCoordinate : IEquatable<CrsCoordinate> {
 
         public CrsProjection CrsProjection { get; private set; }
         
@@ -39,5 +41,37 @@ namespace SwedenCrsTransformations {
         ) {
             return new CrsCoordinate(crsProjection, xLongitude, yLatitude);
         }
+
+        // ----------------------------------------------------------------------------------------------------------------------
+        // These five methods below was generated with Visual Studio 2019
+        public override bool Equals(object obj) {
+            return Equals(obj as CrsCoordinate);
+        }
+
+        public bool Equals(CrsCoordinate other) {
+            return other != null &&
+                   CrsProjection == other.CrsProjection &&
+                   XLongitude == other.XLongitude &&
+                   YLatitude == other.YLatitude;
+        }
+
+        public override int GetHashCode() {
+            int hashCode = 1147467376;
+            hashCode = hashCode * -1521134295 + CrsProjection.GetHashCode();
+            hashCode = hashCode * -1521134295 + XLongitude.GetHashCode();
+            hashCode = hashCode * -1521134295 + YLatitude.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(CrsCoordinate left, CrsCoordinate right) {
+            return EqualityComparer<CrsCoordinate>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(CrsCoordinate left, CrsCoordinate right) {
+            return !(left == right);
+        }
+
+        // These five methods above was generated with Visual Studio 2019
+        // ----------------------------------------------------------------------------------------------------------------------
     }
 }
--- SwedenCrsTransformationsTests/CrsCoordinateTest.cs
+++ SwedenCrsTransformationsTests/CrsCoordinateTest.cs
@@ -28,5 +28,55 @@ namespace SwedenCrsTransformationsTests {
             Assert.AreEqual(y, crsCoordinate.YLatitude);
         }
 
+
+        private const double latitudeStockholmCentralStation = 59.330231;
+        private const double longitudeStockholmCentralStation = 18.059196;
+
+        [Test]
+        public void EqualityTest() {
+            CrsCoordinate coordinateInstance_1 = CrsCoordinate.CreateCoordinatePoint(CrsProjection.wgs84, longitudeStockholmCentralStation, latitudeStockholmCentralStation);
+            CrsCoordinate coordinateInstance_2 = CrsCoordinate.CreateCoordinatePoint(CrsProjection.wgs84, longitudeStockholmCentralStation, latitudeStockholmCentralStation);
+            Assert.AreEqual(coordinateInstance_1, coordinateInstance_2);
+            Assert.AreEqual(coordinateInstance_1.GetHashCode(), coordinateInstance_2.GetHashCode());
+            Assert.IsTrue(coordinateInstance_1 == coordinateInstance_2);
+            Assert.IsTrue(coordinateInstance_2 == coordinateInstance_1);
+            Assert.IsTrue(coordinateInstance_1.Equals(coordinateInstance_2));
+            Assert.IsTrue(coordinateInstance_2.Equals(coordinateInstance_1));
+
+
+            double delta = 0.000000000000001; // see comments further below regarding the value of "delta"
+            CrsCoordinate coordinateInstance_3 = CrsCoordinate.CreateCoordinatePoint(
+                CrsProjection.wgs84,
+                longitudeStockholmCentralStation + delta,
+                latitudeStockholmCentralStation + delta
+            );
+            Assert.AreEqual(coordinateInstance_1, coordinateInstance_3);
+            Assert.AreEqual(coordinateInstance_1.GetHashCode(), coordinateInstance_3.GetHashCode());
+            Assert.IsTrue(coordinateInstance_1 == coordinateInstance_3); // method "operator =="
+            Assert.IsTrue(coordinateInstance_3 == coordinateInstance_1);
+            Assert.IsTrue(coordinateInstance_1.Equals(coordinateInstance_3));
+            Assert.IsTrue(coordinateInstance_3.Equals(coordinateInstance_1));
+
+            // Regarding the chosen value for "delta" (which is added to the lon/lat values, to create a slightly different value) above and below,
+            // it is because of experimentation this "breakpoint" value has been determined, i.e. the above value still resulted in equality 
+            // but when it was increased as below with one decimal then the above kind of assertions failed and therefore the other assertions below 
+            // are used instead e.g. testing the overloaded operator "!=".
+            // You should generally be cautios when comparing floating point values but the above test indicate that values are considered equal even though 
+            // the difference is as 'big' as in the "delta" value above.
+
+            delta = delta * 10; // moving the decimal one bit to get a somewhat larger values, and then the instances are not considered equal, as you can see in the tests below.
+            CrsCoordinate coordinateInstance_4 = CrsCoordinate.CreateCoordinatePoint(
+                CrsProjection.wgs84,
+                longitudeStockholmCentralStation + delta,
+                latitudeStockholmCentralStation + delta
+            );
+            // Note that below are the Are*NOT*Equal assertions made instead of AreEqual as further above when a smaller delta value was used
+            Assert.AreNotEqual(coordinateInstance_1, coordinateInstance_4);
+            Assert.AreNotEqual(coordinateInstance_1.GetHashCode(), coordinateInstance_4.GetHashCode());
+            Assert.IsTrue(coordinateInstance_1 != coordinateInstance_4); // Note that the method "operator !=" becomes used here
+            Assert.IsTrue(coordinateInstance_4 != coordinateInstance_1);
+            Assert.IsFalse(coordinateInstance_1.Equals(coordinateInstance_4));
+            Assert.IsFalse(coordinateInstance_4.Equals(coordinateInstance_1));
+        }
     }
 }
