2733e6921665b72fcd18ed48265dae2916c7b9e2
Tomas Johansson <github-tomas@programmerare.com>
2021-01-12 01:27:38 +0100

    Refactored the method 'Transformer.Transform' to fewer nested if/else statements. Now instead only three top level "if conditions" that selects the TransformStrategy to use.

--- MightyLittleGeodesy/Transformer.cs
+++ MightyLittleGeodesy/Transformer.cs
@@ -19,35 +19,38 @@ namespace MightyLittleGeodesy {
 
             TransformStrategy _transFormStrategy = null;
 
-            if(sourceCoordinate.crsProjection.isWgs84()) {
-                if(targetCrsProjection.isSweref()) {
-                    _transFormStrategy = _transformStrategy_from_WGS84_to_SWEREF99_or_RT90;
-                }
-                else if(targetCrsProjection.isRT90()) {
-                    _transFormStrategy = _transformStrategy_from_WGS84_to_SWEREF99_or_RT90;
-                }
+            // Transform FROM wgs84:
+            if(
+                sourceCoordinate.crsProjection.isWgs84()
+                &&
+                ( targetCrsProjection.isSweref() || targetCrsProjection.isRT90() )
+            ) {
+                _transFormStrategy = _transformStrategy_from_WGS84_to_SWEREF99_or_RT90;
             }
-            else if(sourceCoordinate.crsProjection.isSweref()) {
-                if(targetCrsProjection.isWgs84()) {
-                    _transFormStrategy = _transformStrategy_from_SWEREF99_or_RT90_to_WGS84;
-                }
-                else {
-                    // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
-                    _transFormStrategy = _transFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget;
-                }
+
+            // Transform TO wgs84:
+            else if(
+                targetCrsProjection.isWgs84()
+                &&
+                ( sourceCoordinate.crsProjection.isSweref() || sourceCoordinate.crsProjection.isRT90() )
+            ) {
+                _transFormStrategy = _transformStrategy_from_SWEREF99_or_RT90_to_WGS84;
             }
-            else if(sourceCoordinate.crsProjection.isRT90()) {
-                if(targetCrsProjection.isWgs84()) {
-                    _transFormStrategy = _transformStrategy_from_SWEREF99_or_RT90_to_WGS84;
-                }
-                else {
-                    // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
-                    _transFormStrategy = _transFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget;
-                }
+
+            // Transform between two non-wgs84:
+            else if(
+                ( sourceCoordinate.crsProjection.isSweref() || sourceCoordinate.crsProjection.isRT90() )
+                &&
+                ( targetCrsProjection.isSweref() || targetCrsProjection.isRT90() )
+            ) {
+                // the only direct transform supported is to/from WGS84, so therefore first transform to wgs84
+                _transFormStrategy = _transFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget;
             }
+            
             if(_transFormStrategy != null) {
                 return _transFormStrategy.Transform(sourceCoordinate, targetCrsProjection);
             }
+
             throw new ArgumentException(string.Format("Unhandled source/target EPSG {0} ==> {1}", sourceCoordinate.epsgNumber, targetCrsProjection));
         }
 
