dd86632da6681d76b2e5b1074b3bf45ac93e385c
Tomas Johansson <github-tomas@programmerare.com>
2021-01-14 22:47:37 +0100

    Changed some fields to be declared as 'readonly'

--- SwedenCrsTransformations/Transformation/Transformer.cs
+++ SwedenCrsTransformations/Transformation/Transformer.cs
@@ -15,13 +15,13 @@ namespace SwedenCrsTransformations.Transformation {
     internal class Transformer {
 
         // Implementations of transformations from WGS84:
-        private static TransformStrategy _transformStrategy_from_WGS84_to_SWEREF99_or_RT90 = new TransformStrategy_from_WGS84_to_SWEREF99_or_RT90();
+        private static readonly TransformStrategy _transformStrategy_from_WGS84_to_SWEREF99_or_RT90 = new TransformStrategy_from_WGS84_to_SWEREF99_or_RT90();
 
         // Implementations of transformations to WGS84:
-        private static TransformStrategy _transformStrategy_from_SWEREF99_or_RT90_to_WGS84 = new TransformStrategy_from_SWEREF99_or_RT90_to_WGS84();
+        private static readonly TransformStrategy _transformStrategy_from_SWEREF99_or_RT90_to_WGS84 = new TransformStrategy_from_SWEREF99_or_RT90_to_WGS84();
 
         // Implementation first transforming to WGS84 and then to the real target:
-        private static TransformStrategy _transFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget  = new TransFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget();
+        private static readonly TransformStrategy _transFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget  = new TransFormStrategy_From_Sweref99OrRT90_to_WGS84_andThenToRealTarget();
 
         public static CrsCoordinate Transform(CrsCoordinate sourceCoordinate, CrsProjection targetCrsProjection) {
             if(sourceCoordinate.CrsProjection == targetCrsProjection) return sourceCoordinate;
